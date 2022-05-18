bce5a333d64027dda3ab584c00e44459b41a8391
VolodiaDovban <98911240+dovban46@users.noreply.github.com>
2022-05-18 18:55:33 +0300

    Додання новини

--- database/function.php
+++ database/function.php
@@ -1,6 +1,7 @@
 <?php
 include "conf.php";
 
+//додання меню
 function get_menu (){
     global $conn;
     $sql = "SELECT * FROM menu";
@@ -9,4 +10,15 @@ function get_menu (){
     return $menus;
 }
 $menus = get_menu($conn);
+
+
+//додання новини
+function get_news() {
+    global $conn;
+    $sql = "SELECT * FROM news";
+    $result = mysqli_query($conn, $sql);
+
+    $news = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $news;
+}
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,20 @@
 ?>
 <section>
     <div class="container">
-
+        <div class="row">
+            <div class="col">
+                <?php $news = get_news();?>
+                <?php foreach ($news as $new):?>
+                    <div class="card" style="width: 48rem;" >
+                        <img src="<?=$new['image']?>"  alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?=$new['title']?></h5>
+                            <p class="card-text"><?= mb_substr($new['content'], 0,280, 'UTF-8') . '...'?></p>
+                            <a href="post.php?news_id=<?=$new['id']?>" class="btn btn-primary">Переглянути</a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
 </section>
